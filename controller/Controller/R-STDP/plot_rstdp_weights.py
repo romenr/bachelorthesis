854aaba5f076a4f688c3b0d460aa18112bef6682
René Romen <romen.rene@gmail.com>
2018-07-01 11:33:15 +0200

    Improved graphs

--- controller/Controller/R-STDP/plot_rstdp_weights.py
+++ controller/Controller/R-STDP/plot_rstdp_weights.py
@@ -4,6 +4,7 @@ import numpy as np
 import h5py
 import matplotlib.pyplot as plt
 import parameters as param
+from matplotlib import gridspec
 import argparse
 
 # Configure Command Line interface
@@ -13,32 +14,53 @@ parser.add_argument('-f', '--inputFile', help="Input file", default='./data/rstd
 parser.add_argument('-o', '--outputFile', help="Output file")
 args = parser.parse_args()
 
-# R-STDP weights learned
-# Fig. 5.7, Fig. 5.8, Fig. 5.10
+
+def plot_weights(ax, weights):
+	ax.imshow(weights.astype(int), alpha=0.5)
+	ax.set_yticklabels([])
+	ax.set_xticklabels([])
+	ax.set_yticks([])
+	ax.set_xticks([])
+	for (j, i), label in np.ndenumerate(weights):
+		ax.text(i, j, int(label), ha='center', va='center')
 
 h5f = h5py.File(args.inputFile, 'r')
 
 w_l = np.array(h5f['w_l'], dtype=float)
 w_r = np.array(h5f['w_r'], dtype=float)
-weights_l = w_l[-1].T
-weights_r = w_r[-1].T
-print w_r.shape
+m = w_r.shape[0] / 2
+weights_l0 = w_l[0].T
+weights_r0 = w_r[0].T
+weights_l1 = w_l[m].T
+weights_r1 = w_r[m].T
+weights_l2 = w_l[-1].T
+weights_r2 = w_r[-1].T
 
-fig = plt.figure(figsize=(6,6))
+fig = plt.figure(figsize=(9, 7))
+gs = gridspec.GridSpec(3, 2)
 
-ax1 = plt.subplot(211)
+ax1 = plt.subplot(gs[0, 0])
 plt.title('Left Weights')
-plt.imshow(weights_l, alpha=0.5)
-plt.axis('off')
-for (j,i),label in np.ndenumerate(weights_l):
-	ax1.text(i,j,int(label),ha='center',va='center')
+ax1.set_ylabel('Step 0')
+plot_weights(ax1, weights_l0)
 
-ax2 = plt.subplot(212)
+ax2 = plt.subplot(gs[0, 1])
 plt.title('Right Weights')
-plt.imshow(weights_r, alpha=0.5)
-plt.axis('off')
-for (j,i),label in np.ndenumerate(weights_r):
-	ax2.text(i,j,int(label),ha='center',va='center')
+plot_weights(ax2, weights_r0)
+
+ax3 = plt.subplot(gs[1, 0])
+ax3.set_ylabel('Step '+str(m))
+plot_weights(ax3, weights_l1)
+
+ax4 = plt.subplot(gs[1, 1])
+plot_weights(ax4, weights_r1)
+
+ax5 = plt.subplot(gs[2, 0])
+ax5.set_ylabel('Step '+str(w_r.shape[0]))
+plot_weights(ax5, weights_l2)
+
+ax6 = plt.subplot(gs[2, 1])
+plot_weights(ax6, weights_r2)
 
 fig.tight_layout()
 if args.outputFile is not None:
