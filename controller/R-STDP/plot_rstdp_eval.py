45957f1d00b746203a4bad605e1cc86f4248ee06
René Romen <romen.rene@gmail.com>
2018-07-29 01:44:33 +0200

    Visualized success and failures in duration plot

--- controller/R-STDP/plot_rstdp_eval.py
+++ controller/R-STDP/plot_rstdp_eval.py
@@ -19,6 +19,7 @@ h5f = h5py.File(path.join(args.dir, param.evaluation_file), 'r')
 rewards = np.array(h5f['reward'], dtype=float)
 episode_steps = np.array(h5f["episode_steps"], dtype=int)
 angle_to_target = np.array(h5f['angle_to_target'], dtype=float)
+episode_completed = np.array(h5f['episode_completed'], dtype=bool)
 
 fig = plt.figure(figsize=(12, 6))
 gs = gridspec.GridSpec(3, 2)
@@ -31,7 +32,13 @@ for i in range(angle_to_target.size):
 
 # Plot 1 Plot Steps in Episode i
 ax1 = plt.subplot(gs[0, 0])
-ax1.plot(episode_steps)
+values_x = np.array(range(episode_steps.size))
+success_y = episode_steps[episode_completed]
+success_x = values_x[episode_completed]
+failures_y = episode_steps[~episode_completed]
+failures_x = values_x[~episode_completed]
+ax1.scatter(success_x, success_y, marker='^', color='g')
+ax1.scatter(failures_x, failures_y, marker='x', color='r')
 ax1.set_ylabel("Duration")
 ax1.set_xlabel("Episode")
 
